19c8ff82928f21d8fc2bc64795654bfb6dc1b0a5
Razboy20 <razboy20@gmail.com>
2020-03-02 18:31:13 -0600

    added a 'createInvite' function

--- addons/other.js
+++ addons/other.js
@@ -9,10 +9,13 @@ exports.message = function(client, msg) {
 
 	if (msg.member.roles.cache.some((val) => val.name.containsAny(adminRoles))) {
 		if (command === 'createInvite') {
+			msg.channel.startTyping();
 			msg.channel
 				.createInvite({ maxAge: 0, unique: true })
 				.then(function(invite) {
+					msg.channel.stopTyping(true);
 					console.log(`Created an invite with a code of ${invite.code}`);
+					msg.channel.send(`Invite created!\n${invite.url}`);
 				})
 				.catch(console.error);
 		}
