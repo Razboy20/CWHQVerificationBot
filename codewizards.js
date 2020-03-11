6fc73f56b51c0ddeaf2a06ff7a639e11dfdd3a5e
Razboy20 <razboy20@gmail.com>
2020-03-10 21:31:18 -0500

    added express

--- codewizards.js
+++ codewizards.js
@@ -1,16 +1,105 @@
 const axios = require('axios');
 const Discord = require('discord.js');
+const fs = require('fs');
+const express = require('express');
+const https = require('https');
+const fetch = require('node-fetch');
 require('dotenv').config();
 const client = new Discord.Client();
 
+const app = express();
+
+port = 3750;
+
+const hskey = fs.readFileSync(process.env.HTTPS_KEY);
+const hscert = fs.readFileSync(process.env.HTTPS_CERT);
+const expressOptions = {
+	key: hskey,
+	cert: hscert
+};
+
+server = https.createServer(expressOptions, app);
+
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('How have you found this O_O'));
+app.post('/authuser', async (req, res) => {
+	data = req.body;
+	console.log(data);
+	if (!data.code) {
+		res.status(406).send('No code specified');
+		return;
+	}
+	console.log('Verifying user with code ' + data.code);
+	try {
+		data = {
+			client_id: process.env.APPLICATION_ID,
+			client_secret: process.env.APPLICATION_SECRET,
+			grant_type: 'authorization_code',
+			code: data.code,
+			redirect_uri: 'https://elies.codewizardshq.com/Testing_Folder/discord/redirect.html',
+			scope: 'identify'
+		};
+		headers = {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		};
+
+		var form_data = new URLSearchParams();
+
+		for (var key in data) {
+			form_data.append(key, data[key]);
+		}
+		const tokenResponse = await fetch('https://discordapp.com/api/oauth2/token', {
+			method: 'POST',
+			body: form_data,
+			headers: headers
+		});
+		const tokenInfo = await tokenResponse.json();
+
+		if (!tokenInfo.access_token) {
+			res.status(407).send('An error occured.');
+			return;
+		}
+		const userResponse = await fetch('https://discordapp.com/api/users/@me', {
+			headers: { Authorization: 'Bearer ' + tokenInfo.access_token }
+		});
+		const userData = await userResponse.json();
+		res.send(userData);
+		const user = client.guilds.cache.get(guildId).members.cache.get(userData.id);
+		// console.log(user);
+
+		user.send(createEmbeds(user.user.username).checking).then(function(message) {
+			let role = client.guilds.cache.get(guildId).roles.cache.find((role) => role.name === giveRoleName);
+			client.guilds.cache.get(guildId).members.cache.get(user.id).roles.add(role).catch(console.error);
+
+			let role2 = client.guilds.cache.get(guildId).roles.cache.find((role) => role.name === removeRoleName);
+			client.guilds.cache.get(guildId).members.cache.get(user.id).roles.remove(role2).catch(console.error);
+			message.edit(createEmbeds(user.user.username).verified);
+			client.guilds.cache
+				.get(guildId)
+				.channels.cache.find((val) => val.name === 'verify-log')
+				.send(createEmbeds(user.user.username).isverified);
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(407).send('An error occured.');
+		return;
+	}
+});
+server.listen(port, () => console.log(`Discord Bot listening on port ${port}!`));
 // get addons -- Loader made by tbranyen
 
-var addons = [];
+let addons = [];
 
-var normalizedPath = require('path').join(__dirname, 'addons');
+const normalizedPath = require('path').join(__dirname, 'addons');
 
 require('fs').readdirSync(normalizedPath).forEach(function(file) {
-	if (file.substr(-3) == '.js') addons.push(require('./addons/' + file));
+	if (file.substr(-3) === '.js') addons.push(require('./addons/' + file));
 });
 
 const guildId = process.env.GUILD_ID;
@@ -27,7 +116,7 @@ client.on('ready', () => {
 		type: 'WATCHING'
 	});
 	addons.forEach((addon) => {
-		if (addon.ready) addon.ready(client);
+		if (addon.ready) addon.ready();
 	});
 });
 
@@ -84,6 +173,31 @@ function createEmbeds(name, _id) {
 				file: undefined
 			}
 		},
+		sendfailed: {
+			embed: {
+				title: '@' + name + ', Error, cannot send DM.',
+				description: "Please allow DM's from users in your security settings!",
+				url: undefined,
+				color: 16724736,
+				author: {
+					name: 'Code Wizard',
+					icon_url:
+						'https://cdn.discordapp.com/avatars/623314619154300988/539bf23444c28b765923eb5b40e9f859.png',
+					url: undefined
+				},
+				timestamp: undefined,
+				fields: [],
+				thumbnail: {
+					url: 'https://cdn.discordapp.com/avatars/623314619154300988/539bf23444c28b765923eb5b40e9f859.png'
+				},
+				image: undefined,
+				footer: {
+					text: 'Code Wizard Verification Bot made by Razboy20',
+					icon_url: undefined
+				},
+				file: undefined
+			}
+		},
 		isverifying: {
 			embed: {
 				title: '@' + name + ' started the verification process.',
@@ -111,7 +225,7 @@ function createEmbeds(name, _id) {
 				description:
 					'To verify that you have a CodeWizardsHQ Account, \n\n1) go to one of your webpages on your **CodeWizards** editor and paste ```<id verificationnum="cw' +
 					_id +
-					'"/>``` somewhere on the page (preferably in the `<head>` tags if you have one). \n\n2) Afterwards, paste in the website link below in a DM replying to me.',
+					'"/>``` somewhere on the page \n\n2) Afterwards, paste in the website link below in a DM replying to me.',
 				url: undefined,
 				color: 16777011,
 				author: {
@@ -208,6 +322,7 @@ function createEmbeds(name, _id) {
 	};
 }
 client.on('message', (msg) => {
+	if (msg.author.id === client.user.id) return;
 	try {
 		addons.forEach((addon) => {
 			if (addon.message) addon.message(client, msg);
@@ -216,39 +331,39 @@ client.on('message', (msg) => {
 		console.log(err);
 	}
 
-	if (msg.channel.type == 'text') {
-		if (msg.channel.name != process.env.VERIFY_CHANNEL_NAME) {
+	if (msg.channel.type === 'text') {
+		if (msg.channel.name !== process.env.VERIFY_CHANNEL_NAME) {
 			return;
 		}
 
-		if (msg.content == '!verify') {
+		if (msg.content === '!verify') {
 			msg.channel.send(createEmbeds(msg.member.displayName).toverify).then(function(message) {
-				setTimeout(() => {
-					message.delete();
-				}, 10000);
+				// send dm
+				msg.author.send(createEmbeds(msg.author.username, msg.author.id).toverifydm).catch(() => {
+					// if fails, edit message to be 'sendFailed'
+					message.edit(createEmbeds(msg.member.displayName).sendfailed);
+				});
+				message.delete({ timeout: 10000 });
 			});
 			msg.guild.channels.cache
 				.find((val) => val.name === 'verify-log')
 				.send(createEmbeds(msg.member.displayName).isverifying);
-			msg.author.send(createEmbeds(msg.author.username, msg.author.id).toverifydm);
 		}
-		if (msg.content == '!clear50') {
+		if (msg.content === '!clear50') {
 			msg.channel.bulkDelete(50).catch(console.error);
 		}
 		// if (msg.author.id == '250809865767878657') {
 		// 	return;
 		// }
-		setTimeout(() => {
-			msg.delete();
-		}, 3000);
+		if (msg.author.id !== client.user.id) msg.delete({ timeout: 3000 });
 	}
 
 	//checking if author is a bot
-	if (msg.author.bot == true) {
+	if (msg.author.bot === true) {
 		return;
 	}
 
-	if (msg.channel.type == 'dm') {
+	if (msg.channel.type === 'dm') {
 		msg.channel.send(createEmbeds(msg.author.username).checking).then(function(message) {
 			if (!msg.content.includes('codewizardshq.com')) {
 				message.edit(createEmbeds(msg.author.username).declined);
@@ -274,9 +389,9 @@ client.on('message', (msg) => {
 							let role2 = client.guilds.cache
 								.get(guildId)
 								.roles.cache.find((role) => role.name === removeRoleName);
-							client.guilds
+							client.guilds.cache
 								.get(guildId)
-								.members.get(msg.author.id)
+								.members.cache.get(msg.author.id)
 								.roles.remove(role2)
 								.catch(console.error);
 							message.edit(createEmbeds(msg.author.username).verified);
@@ -301,6 +416,7 @@ client.on('guildMemberAdd', (member) => {
 });
 
 client.on('messageUpdate', (oldmsg, newmsg) => {
+	if (oldmsg.author.id === client.user.id) return;
 	try {
 		addons.forEach((addon) => {
 			if (addon.messageEdit) addon.messageEdit(client, oldmsg, newmsg);
